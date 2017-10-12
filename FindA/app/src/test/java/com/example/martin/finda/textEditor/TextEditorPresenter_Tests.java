14a59c0fd12b36d8b8c12defd148ce8de2a72f2c
Martin Kamenov <martykam@abv.bg>
2017-10-12 08:23:27 +0300

    Added textEditorPresenter tests.

---
+++ FindA/app/src/test/java/com/example/martin/finda/textEditor/TextEditorPresenter_Tests.java
@@ -0,0 +1,38 @@
+package com.example.martin.finda.textEditor;
+
+import com.example.martin.finda.base.BaseContracts;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.mockito.Mock;
+
+/**
+ * Created by Martin on 12.10.2017 г..
+ */
+
+public class TextEditorPresenter_Tests {
+    @Mock
+    TextEditorActivity activity;
+    @Mock
+    BaseContracts.View view;
+
+    @Test
+    public void ConstructorShould_CreateInstance() {
+        // Arrange and Act
+        TextEditorPresenter presenter = new TextEditorPresenter(activity);
+
+        // Assert
+        Assert.assertNotNull(presenter);
+    }
+
+    @Test
+    public void SubscribeShould_AddView() {
+        // Arrange
+        TextEditorPresenter presenter = new TextEditorPresenter(activity);
+        // Act
+        presenter.subscribe(view);
+        // Assert
+        Assert.assertSame(presenter.mView, view);
+    }
+}
