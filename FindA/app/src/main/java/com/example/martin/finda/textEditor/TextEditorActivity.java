b779378edeb0c769d82b88fc731e75ced6e15bfb
Martin Kamenov <martykam@abv.bg>
2017-10-14 10:00:36 +0300

    Fixed TextEditorActivity. Now one can use camera and gallery.

--- FindA/app/src/main/java/com/example/martin/finda/textEditor/TextEditorActivity.java
+++ FindA/app/src/main/java/com/example/martin/finda/textEditor/TextEditorActivity.java
@@ -26,8 +26,8 @@ public class TextEditorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_text);
 
-        /*mTextEditorFragment = new TextEditorFragment();
-        mHttpRequester = new HttpRequester();
+        mTextEditorFragment = new TextEditorFragment();
+        /*mHttpRequester = new HttpRequester();
         mPresenter = new TextEditorPresenter(mHttpRequester);
         mTextEditorFragment.setPresenter(mPresenter);*/
 
@@ -36,32 +36,4 @@ public class TextEditorActivity extends AppCompatActivity {
                 .replace(R.id.text_container, mTextEditorFragment)
                 .commit();
     }
-
-    /*public void makeHttp() {
-        Request request = new Request.Builder().url("https://fierce-crag-61509.herokuapp.com/users")
-                .build();
-        client = new OkHttpClient();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(Call call, final Response response) throws IOException {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            Toast.makeText(TextEditorActivity.this, response.body().string(), Toast.LENGTH_SHORT).show();
-                            //findViewById(R.id.loadingPanel).setVisibility(View.GONE);
-                        } catch (IOException e) {
-                            Toast.makeText(TextEditorActivity.this, "something went wrong!", Toast.LENGTH_SHORT).show();
-                            //findViewById(R.id.loadingPanel).setVisibility(View.GONE);
-                        }
-                    }
-                });
-            }
-        });
-    }*/
 }
